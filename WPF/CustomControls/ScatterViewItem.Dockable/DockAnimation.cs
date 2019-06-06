2a66d7f89593b2eab1783791b522f8050c944b3b
Matt Bielman <mattjb0@hotmail.com>
2019-06-06 00:22:00 -0700

    Created DemoApp to showcase custom controls. Created DockableScatterViewItem control

---
+++ WPF/CustomControls/ScatterViewItem.Dockable/DockAnimation.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Bielman.SUR40.Controls
+{
+    public class DockAnimation
+    {
+        private DockableScatterViewItem _dockableScatterViewItem;
+
+        private Storyboard _dockStoryboard;
+        public Storyboard DockStoryboard
+        {
+            get { return _dockStoryboard; }
+            private set { _dockStoryboard = value; }
+        }
+
+        private DoubleAnimation _orientationAnimation;
+        public DoubleAnimation OrientationAnimation
+        {
+            get { return _orientationAnimation; }
+            private set { _orientationAnimation = value; }
+        }
+
+        private PointAnimation _positionAnimation;
+        public PointAnimation PositionAnimation
+        {
+            get { return _positionAnimation; }
+            private set { _positionAnimation = value; }
+        }
+
+        private float _storyboardDuration;
+        public float StoryboardDuration
+        {
+            get { return _storyboardDuration; }
+            set { _storyboardDuration = value; }
+        }
+
+        private float _targetOrientation;
+        private Point _targetPosition;
+
+        /// <summary>
+        /// We construct the docking animation in code since dynamic 
+        /// property binding support is limited in user controls
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="durationInMilliseconds"></param>
+        public DockAnimation(DockableScatterViewItem target, float durationInMilliseconds)
+        {
+            _dockableScatterViewItem = target;
+
+            StoryboardDuration = durationInMilliseconds;
+
+            DockStoryboard = new Storyboard();
+            DockStoryboard.FillBehavior = FillBehavior.Stop;
+            DockStoryboard.Completed += DockStoryboard_Completed;
+
+            OrientationAnimation = new DoubleAnimation();
+            DockStoryboard.Children.Add(OrientationAnimation);
+
+            PositionAnimation = new PointAnimation();
+            DockStoryboard.Children.Add(PositionAnimation);
+
+            Storyboard.SetTarget(OrientationAnimation, target);
+            Storyboard.SetTargetProperty(OrientationAnimation, new PropertyPath(DockableScatterViewItem.OrientationProperty));
+
+            Storyboard.SetTarget(PositionAnimation, target);
+            Storyboard.SetTargetProperty(PositionAnimation, new PropertyPath(DockableScatterViewItem.CenterProperty));
+        }
+
+        public virtual void Play(float _newOrientation, Point _newPosition)
+        {
+            // Store final orientation and position so we can apply it after animating
+            _targetOrientation = _newOrientation;
+            _targetPosition = _newPosition;
+
+            // Align ScatterViewItem to the docked edge
+            OrientationAnimation.From = _dockableScatterViewItem.Orientation;
+            OrientationAnimation.To = _newOrientation;
+            OrientationAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(StoryboardDuration));
+
+            // Partially tuck the ScatterViewItem under the docked edge
+            PositionAnimation.From = _dockableScatterViewItem.Center;
+            PositionAnimation.To = _newPosition;
+            PositionAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(_storyboardDuration));
+
+            _dockStoryboard.Begin();
+        }
+
+        /// <summary>
+        /// Apply final resting orientation and position
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DockStoryboard_Completed(object sender, EventArgs e)
+        {
+            _dockableScatterViewItem.Orientation = _targetOrientation;
+            _dockableScatterViewItem.Center = _targetPosition;
+        }
+    }
+}
---
+++ WPF/CustomControls/ScatterViewItem.Dockable/DockableScatterViewItem.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using Microsoft.Surface.Presentation.Controls;
+using System.Windows.Input;
+
+namespace Bielman.SUR40.Controls
+{
+    public partial class DockableScatterViewItem : ScatterViewItem
+    {
+        private delegate void UpdateItemPosition();
+
+        public delegate void OnDockChangedEventHandler(object sender, EventArgs args);
+        public event OnDockChangedEventHandler OnDockChanged;
+        
+        public enum DockLocationEnum { None, Left, Right, Top, Bottom };
+
+        private bool _isDockable = true;
+        public bool IsDockable
+        {
+            get { return _isDockable; }
+            set { _isDockable = value; }
+        }
+
+        private Thickness _dockThickness = new Thickness(64);
+        public Thickness DockThickness
+        {
+            get { return _dockThickness; }
+            set { _dockThickness = value; }
+        }
+
+        private Thickness _dockPadding = new Thickness(64);
+        public Thickness DockPadding
+        {
+            get { return _dockPadding; }
+            set { _dockPadding = value; }
+        }
+
+        private DockLocationEnum _prevDockedLocation = DockLocationEnum.None;
+        private DockLocationEnum _dockedLocation = DockLocationEnum.None;
+        public DockLocationEnum DockedLocation
+        {
+            get { return _dockedLocation; }
+            private set { _dockedLocation = value; }
+        }
+
+        private BackgroundWorker _worker;
+        
+        private DockAnimation _dockAnimation;
+
+        private float _dockAnimationDuration = 500;
+        public float DockAnimationDuration
+        {
+            get { return _dockAnimationDuration; }
+            set { _dockAnimationDuration = value; }
+        }
+
+        public DockableScatterViewItem()
+        {
+            InitializeComponents();
+        }
+
+        /// <summary>
+        /// Check ScatterViewItem position against dock thickness and dock it if it's close to the edge of the screen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            _prevDockedLocation = DockedLocation;
+
+            this.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, (UpdateItemPosition)delegate ()
+            {
+                this.UpdateLayout();
+
+                // Docked right
+                if (this.Center.X >= (this.Parent as ScatterView).ActualWidth - this.DockThickness.Right)
+                {
+                    this.DockedLocation = DockLocationEnum.Right;
+                    _dockAnimation.Play(270, new Point((this.Parent as ScatterView).ActualWidth + (this.ActualHeight / 2) - (this.DockPadding.Right), this.Center.Y));
+                }
+                // Docked left
+                else if (this.Center.X <= this.DockThickness.Left)
+                {
+                    this.DockedLocation = DockLocationEnum.Left;
+                    _dockAnimation.Play(90, new Point(((this.ActualHeight / 2) - this.DockPadding.Left) * -1, this.Center.Y));
+                }
+                // Docked top
+                else if (this.Center.Y <= this.DockThickness.Top)
+                {
+                    this.DockedLocation = DockLocationEnum.Top;
+                    _dockAnimation.Play(180, new Point(this.Center.X, this.DockPadding.Top - (this.ActualHeight / 2)));
+                }
+                // Docked bottom
+                else if (this.Center.Y >= (this.Parent as ScatterView).ActualHeight - this.DockThickness.Bottom)
+                {
+                    this.DockedLocation = DockLocationEnum.Bottom;
+                    _dockAnimation.Play(0, new Point(this.Center.X, (this.Parent as ScatterView).ActualHeight + (this.ActualHeight / 2) - this.DockPadding.Bottom));
+                }
+                // Not docked
+                else
+                {
+                    this.DockedLocation = DockLocationEnum.None;
+                }
+            });
+        }
+
+        /// <summary>
+        /// Check and apply docking after user releases ScatterViewItem and it comes to a stop.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnManipulationCompleted(ManipulationCompletedEventArgs e)
+        {
+            if ((_worker.IsBusy == false) && (this.IsDockable == true))
+            {
+                _worker.RunWorkerAsync();
+            }
+            base.OnManipulationCompleted(e);
+        }
+
+        private void InitializeComponents()
+        {
+            _worker = new BackgroundWorker();
+            _worker.DoWork += new DoWorkEventHandler(_worker_DoWork);
+            _worker.WorkerReportsProgress = false;
+            _worker.WorkerSupportsCancellation = true;
+            _worker.RunWorkerCompleted += _worker_RunWorkerCompleted;
+
+            _dockAnimation = new DockAnimation(this, _dockAnimationDuration);
+        }
+
+        private void _worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (this.DockedLocation != _prevDockedLocation)
+            {
+                if (OnDockChanged != null)
+                {
+                    OnDockChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
